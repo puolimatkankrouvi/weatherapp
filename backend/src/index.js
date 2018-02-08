4eb46ec9ae9e829fdde34f4aa4a6f4f88f7eb9ce
puolimatkankrouvi <cde_1414@yahoo.com>
2018-02-08 12:49:12 +0200

    eslint errors fixed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,4 @@
-const debug = require('debug')('weathermap');
+// const debug = require('debug')('weathermap');
 
 const Koa = require('koa');
 const router = require('koa-router')();
@@ -6,8 +6,8 @@ const fetch = require('node-fetch');
 const cors = require('kcors');
 
 const appId = process.env.APPID || 'a0903457a9bebf5ad5f207cd4b267ea3';
-const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
-const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
+const mapURI = process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
+const targetCity = process.env.TARGET_CITY || 'Helsinki,fi';
 
 const port = process.env.PORT || 9000;
 
@@ -16,38 +16,35 @@ const app = new Koa();
 app.use(cors());
 
 const fetchWeather = async () => {
-  /* 
+  // Uses geolocation longitude and latitude if available
+  /*
+  const geolocation = await getGeolocation();
+  if(geolocation){
+    latitude = geolocation.coords.latitude;
+    longitude = geolocation.coords.longitude;
+    endpoint = `${mapURI}/forecast?lat=${latitude}&lon={longitude}&appid=${appId}&`;
+  }
+  */
+
+  /*
     Now retrieves five day forecast JSON, instead of current weather
-    and uses geolocation longitude and latitude if avalable
    */
-  //const geolocation = await getGeolocation();
-
   const endpoint = `${mapURI}/forecast?q=${targetCity}&appid=${appId}&`;
-
 
   const response = await fetch(endpoint);
 
-  return response ? response.json() : {}
+  return response ? response.json() : {};
 };
 
-/*var getGeolocation = async() => {
-  if("geolocation" in navigator){
-  	navigator.geolocation.getCurrentPosition( (position) => {
-  		return position;
-  	});
-  }
-  else{
-    return {};
-  }
-};*/
+var getGeolocation = () => {
+  return {};
+};
 
 router.get('/api/weather', async ctx => {
   const weatherData = await fetchWeather();
 
   ctx.type = 'application/json; charset=utf-8';
-  /*
-  	The second element of list-array parameter contains the weather few hours from now
-  	*/
+  // The second element of list-array parameter contains the weather few hours from now
   ctx.body = weatherData.list ? weatherData.list[1].weather[0] : {};
 });
 
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -17,23 +17,30 @@ const getWeatherFromApi = async () => {
 class Weather extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      icon: "",
+      icon: '',
+      description: '',
     };
   }
 
   async componentWillMount() {
     const weather = await getWeatherFromApi();
-    this.setState({icon: weather.icon.slice(0, -1)});
+    this.setState({ icon: weather.icon.slice(0, -1) });
+    this.setState({ description: weather.description });
   }
 
   render() {
-    const { icon } = this.state;
-
+    const { icon } = this.state.icon;
     return (
+      /*
       <div className="icon">
         { icon && <img src={`/img/${icon}.svg`} /> }
+      </div>
+      */
+
+      <div>
+        {icon}
+        <p>{ this.state.description}</p>
       </div>
     );
   }
